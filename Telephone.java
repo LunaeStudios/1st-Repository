aea3ee7e2529730726d0c7aa6da9fa3ff17675ee
GradIV <73103125+GradIV@users.noreply.github.com>
2020-10-20 23:54:45 +0530

    Add files via upload
    basic telephone bill code

---
+++ Telephone.java
@@ -0,0 +1,40 @@
+import java.util.*;
+class Telephone
+{
+ int calls;
+ double amt,total;
+ String name;
+ void input()
+ {
+ Scanner sc = new Scanner (System.in);
+ System.out.println("Enter the calls being made");
+  calls = sc.nextInt ();
+ System.out.println("Enter the name of the Customer");
+  name = sc.next ();
+  sc.close();
+ }
+ void calc()
+ {
+  if (calls == 150)
+  {
+   amt = 150;
+   total = 150;
+  }
+  else if (calls == 300)
+  {
+   amt = 300*2;
+   total = amt+150;}
+  }
+   void display()
+   {
+    System.out.println("The name of customer is"+name);
+    System.out.println("the calls being made are"+calls);
+    System.out.println("the total is"+total);
+   }
+   public static void main(String[] args)
+   {
+    Telephone a = new Telephone();
+    a.input ();
+    a.calc();
+    a.display();
+   }}
